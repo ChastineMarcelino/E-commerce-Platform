02dea5fa007ab99babb9cc8daaab535403bde391
Chastine Marcelino <chastmarcelino@gmail.com>
2025-05-22 10:38:12 +0800

    changes

--- ecommerce-api.CLIENT/src/app/pages/forgot-password/forgot-password.component.ts
+++ ecommerce-api.CLIENT/src/app/pages/forgot-password/forgot-password.component.ts
@@ -5,13 +5,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss'],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatSnackBarModule],
 })
 export class ForgotPasswordComponent {
   forgotForm: FormGroup;
@@ -20,7 +22,8 @@ export class ForgotPasswordComponent {
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private http: HttpClient
+    private http: HttpClient,
+     private snackBar: MatSnackBar
   ) {
     this.forgotForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -34,7 +37,7 @@ export class ForgotPasswordComponent {
     const { email, currentPassword, newPassword, confirmPassword } = this.forgotForm.value;
 
     if (newPassword !== confirmPassword) {
-      alert("New Password and Confirm Password do not match!");
+this.snackBar.open("New Password and Confirm Password do not match!", 'Close', { duration: 3000 });
       return;
     }
 
@@ -53,7 +56,8 @@ export class ForgotPasswordComponent {
           confirmPassword,
         }).subscribe({
           next: (res: any) => {
-            alert(res.message || 'OTP sent to email');
+                    this.snackBar.open(res.message || 'OTP sent to email', 'Close', { duration: 3000 });
+
 
             // Save info for OTP page
             localStorage.setItem('resetEmail', email);
@@ -62,13 +66,13 @@ export class ForgotPasswordComponent {
             this.router.navigate(['/verify-otp']);
           },
           error: (err) => {
-            alert(err.error.message || 'Error sending OTP');
+            this.snackBar.open(err.error.message || 'Error sending OTP', 'Close', { duration: 3000 });
             this.isLoading = false;
           }
         });
       },
       error: (err) => {
-        alert(err.error.message || 'Current password does not match last 3 months');
+      this.snackBar.open(err.error.message || 'Current password does not match last 3 months', 'Close', { duration: 3000 });
         this.isLoading = false;
       }
     });
